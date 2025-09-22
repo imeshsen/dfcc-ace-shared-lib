3e7b835cc81904bc099e3be378180e5047f7f004
Imesh Senanayake <115970061+imeshsen@users.noreply.github.com>
2025-09-22 11:30:08 +0530

    Update buildBar.groovy

--- vars/buildBar.groovy
+++ vars/buildBar.groovy
@@ -1,1 +1,13 @@
 
+def call() {
+    echo "Building BAR file using ibmint to echo  ${env.WORKSPACE}..."
+                sh """
+                    docker run --rm \\
+                    --volumes-from jenkins \\
+                    ibmint:latest package \\
+                    --input-path ${env.WORKSPACE} \\
+                    --output-bar-file ${env.WORKSPACE}/MyIntegrationTestProject.bar \\
+                    --do-not-compile-java
+                """
+                echo "BAR file built."
+}
