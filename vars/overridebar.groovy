892aba5cdb7a52bf7f4a964d7c989296594d0b9a
Imesh Senanayake <115970061+imeshsen@users.noreply.github.com>
2025-10-07 10:47:30 +0530

    Update overridebar.groovy

--- vars/overridebar.groovy
+++ vars/overridebar.groovy
@@ -1,4 +1,12 @@
-ibmint apply overrides \
-  --input-bar-file OrderApp.bar \
-  --overrides-file dev-overrides.yaml \
-  --output-bar-file OrderApp_dev.bar
+def call() {
+    echo "Overriding BAR file using ibmint to ${env.WORKSPACE}..."
+                sh """
+                    docker run --rm \\
+                    --volumes-from jenkins \\
+                    ibmint:latest apply overrides \\
+                    --input-bar-file ${env.WORKSPACE}/MyIntegrationTestProject.bar \\
+                    --overrides-file dev-overrides.yaml
+                    --output-bar-file ${env.WORKSPACE}/override.bar
+                """
+                echo "BAR file override complete."
+}
