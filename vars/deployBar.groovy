c09c0009adb91ed000e583f64d97f14a873076d2
Imesh Senanayake <115970061+imeshsen@users.noreply.github.com>
2025-10-14 13:59:07 +0530

    Update deployBar.groovy

--- vars/deployBar.groovy
+++ vars/deployBar.groovy
@@ -11,7 +11,7 @@ def call(String environment) {
         docker run --rm \\
         --volumes-from jenkins \\
         ibmint:latest deploy \\
-        --input-bar-file ${env.WORKSPACE}/${name}.bar \\
+        --input-bar-file ${env.WORKSPACE}/${name}-override.bar \\
         --output-host ${config['host']} \\
         --output-port ${config['port']} \\
         --output-server ${config['server']}
