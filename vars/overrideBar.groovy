3ff0b316662dae937d82775d24fa1ca86dd05b36
Imesh Senanayake <115970061+imeshsen@users.noreply.github.com>
2025-10-22 14:15:38 +0530

    updated

--- vars/overrideBar.groovy
+++ vars/overrideBar.groovy
@@ -1,21 +1,3 @@
-// def call() {
-//     echo "Overriding BAR file using ibmint to ${env.WORKSPACE}..."
-//                 sh """
-//                     docker run --rm \\
-//                     --volumes-from jenkins \\
-//                     ibmint:latest apply overrides \\
-//                     --input-bar-file ${env.WORKSPACE}/MyIntegrationTestProject.bar \\
-//                     --overrides-file dev-overrides.yaml
-//                     --output-bar-file ${env.WORKSPACE}/override.bar
-//                 """
-//                 echo "BAR file override complete."
-// }
-
-
-// docker run --rm -v /home/imesh/myStuff/ibmovride:/app ibmint:latest apply overrides /app/overrides.txt --input-bar-file /app/MyIntegrationTestProject.bar \\
-// --output-bar-file /app/MyIntegrationTestProject_updated.bar
-
-
 def call(String envName) {
     echo "Overriding BAR file using ibmint for environment: ${envName}"
 
@@ -38,4 +20,3 @@ def call(String envName) {
 
     echo "BAR file override complete for ${envName}."
 }
-
